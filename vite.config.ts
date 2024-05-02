aef8b4ef518e79ef47ce502bde5332349678908f
vikas kumar <vk.vikas3003@gmail.com>
2024-05-02 19:12:31 +0530

    configs change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "quarks-ui-components",
+      fileName: "quarks-ui-components",
+    },
+
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
