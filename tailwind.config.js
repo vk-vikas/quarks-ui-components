929167c8c560ef0bac53ebac0c35a0a9e59ff746
vikas kumar <vk.vikas3003@gmail.com>
2024-06-07 16:30:31 +0530

    add safelist to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,16 @@ export default {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    {
+      pattern:
+        /(bg|text|border-t)-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(100|200|300|400|500|600|700|800|900)/,
+    },
+    {
+      pattern: /bg-(black|white)/,
+    },
+    {
+      pattern: /text-(black|white)/,
+    },
+  ],
 };
