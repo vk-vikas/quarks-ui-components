0b49e1e04f2ff6ae3e950e49c5bdb427583ca133
vikas kumar <vk.vikas3003@gmail.com>
2024-06-16 10:10:51 +0530

    add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
@@ -24,5 +26,12 @@ export default defineConfig({
         },
       },
     },
+  },
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    css: true,
   },
 });
